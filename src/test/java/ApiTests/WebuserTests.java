92bc5538095a2f70ecb35dacf8a43b345dbaada4
SumitMahajan <sumit@stagwell.tech>
2017-07-05 23:27:01 -0400

    Initial commit

---
+++ src/test/java/ApiTests/WebuserTests.java
@@ -0,0 +1,7 @@
+package ApiTests;
+
+/**
+ * Created by Chetna on 7/5/2017.
+ */
+public class WebuserTests {
+}
---
+++ src/test/java/Common/Resources.java
@@ -0,0 +1,7 @@
+package Common;
+
+/**
+ * Created by Chetna on 7/5/2017.
+ */
+public class Resources {
+}
